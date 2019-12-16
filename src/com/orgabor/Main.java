31382424781c4f17cc08bfaf276e14d2031a1bc4
Gabor <orbangabeszb@gmail.com>
2019-12-16 15:58:09 +0100

    server ready to receive messages

--- src/com/orgabor/Main.java
+++ src/com/orgabor/Main.java
@@ -1,6 +1,9 @@
 package com.orgabor;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -10,9 +13,20 @@ public class Main {
 		try(ServerSocket serverSocket = new ServerSocket(5000)) {
 			Socket socket = serverSocket.accept();
 			System.out.println("Client connected");
+			BufferedReader input = new BufferedReader(
+								   new InputStreamReader(socket.getInputStream()));
+			PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+			
+			while(true) {
+				String echoString = input.readLine();
+				if(echoString.equals("exit")) {
+					break;
+				}
+				output.println("Echo from server: " + echoString);
+			}
 			
 		} catch(IOException e) {
-			e.printStackTrace();
+			System.out.println("Server exception: " + e.getMessage());
 		}
 
 	}
