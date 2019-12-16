a0978c14361b9fa42182a5e6a37864c2529bdcb8
Gabor <orbangabeszb@gmail.com>
2019-12-16 17:05:31 +0100

    socket, input, output moved to infinite loop

--- src/com/orgabor/Main.java
+++ src/com/orgabor/Main.java
@@ -11,13 +11,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		try(ServerSocket serverSocket = new ServerSocket(5000)) {
-			Socket socket = serverSocket.accept();
-			System.out.println("Client connected");
-			BufferedReader input = new BufferedReader(
-								   new InputStreamReader(socket.getInputStream()));
-			PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-			
 			while(true) {
+				Socket socket = serverSocket.accept();
+				System.out.println("Client connected");
+				BufferedReader input = new BufferedReader(
+									   new InputStreamReader(socket.getInputStream()));
+				PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+				
 				String echoString = input.readLine();
 				if(echoString.equals("exit")) {
 					break;
