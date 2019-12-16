33d1b0803a75457d3a211bb444d6532d7eea54e9
Gabor <orbangabeszb@gmail.com>
2019-12-16 13:15:16 +0100

    initial commit

---
+++ src/com/orgabor/Main.java
@@ -0,0 +1,20 @@
+package com.orgabor;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+	public static void main(String[] args) {
+		try(ServerSocket serverSocket = new ServerSocket(5000)) {
+			Socket socket = serverSocket.accept();
+			System.out.println("Client connected");
+			
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
